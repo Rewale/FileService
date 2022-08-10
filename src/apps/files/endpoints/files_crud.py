81e49f1583738adfa5f6ef494e136d310d64518b
Rewale <komaroff.sania2015@gmail.com>
2022-08-10 18:09:43 +0500

    change routes

--- src/apps/files/endpoints/files_crud.py
+++ src/apps/files/endpoints/files_crud.py
@@ -9,16 +9,16 @@ from .. import services, models, schemas
 files_crud_router = APIRouter()
 
 
-@files_crud_router.post('/upload_file', response_model=schemas.CreatedFileInfo)
+@files_crud_router.post('/', response_model=schemas.CreatedFileInfo)
 async def upload_file(file: UploadFile, background_task: BackgroundTasks):
     return await services.upload_file(file, background_task)
 
 
-@files_crud_router.get('/get_file', response_class=FileResponse)
+@files_crud_router.get('/', response_class=FileResponse)
 async def get_file(file_id: str):
     return await services.get_file_path(file_md5=file_id)
 
 
-@files_crud_router.get('/get_file/b64', response_model=schemas.FileB64Response)
+@files_crud_router.get('/b64', response_model=schemas.FileB64Response)
 async def get_file_b64(file_id: str):
     return await services.get_file_b64(file_id)
